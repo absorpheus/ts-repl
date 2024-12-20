0a63eb1ba672d75e4af7b9491c74f11fe90826b6
Abdullah Malik <absorpheus@gmail.com>
2024-12-20 19:17:24 +0000

    add playground

---
+++ src/index.ts
@@ -0,0 +1,14 @@
+// Welcome to the TypeScript Playground, this is a local environment
+// which gives you a chance to write, share and learn TypeScript.
+
+// You could think of it in three ways:
+//
+//  - A tool for learning TypeScript on your local setup
+//  - A sandbox for experimenting with TypeScript syntax and features
+//  - A way to share your experiments and repositories with others
+
+const anExampleVariable = "Hello World"
+console.log(anExampleVariable)
+
+// To get started, remove these comments and start exploring.
+// The world of TypeScript is yours to discover!
